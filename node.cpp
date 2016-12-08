46c494c135e2f7184897ba849486dcf0591d5419
quiquealon <quiquealonwow@gmail.com>
2016-12-08 10:37:58 -0500

    Color

--- node.cpp
+++ node.cpp
@@ -130,18 +130,18 @@ void Node::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWid
     if (option->state & QStyle::State_Sunken) {
         gradient.setCenter(3, 3);
         gradient.setFocalPoint(3, 3);
-        gradient.setColorAt(1, QColor(Qt::yellow).light(120));
-        gradient.setColorAt(0, QColor(Qt::darkYellow).light(120));
+        gradient.setColorAt(1, QColor(Qt::green).light(120));
+        gradient.setColorAt(0, QColor(Qt::darkGreen).light(120));
     } else {
-        gradient.setColorAt(0, Qt::yellow);
-        gradient.setColorAt(1, Qt::darkYellow);
+        gradient.setColorAt(0, Qt::green);
+        gradient.setColorAt(1, Qt::darkGreen);
     }
     painter->setBrush(gradient);
 
     painter->setPen(QPen(Qt::black, 0));
     painter->drawEllipse(-10, -10, 20, 20);
 
-    painter->setPen(QPen(Qt::blue));
+    painter->setPen(QPen(Qt::black));
     painter->drawText(-3,4,name);
 
 }
